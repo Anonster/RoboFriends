554639fcc440e5bdd4231d7bc943cafb10407c5d
Anonster <sameerkatija@gmail.com>
2020-04-24 16:28:48 +0500

    Updates

---
+++ precache-manifest.4365604e623b75773e54ea18dd9a1279.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc90439f3a381079d772e31e2ed063c",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "449de10d7a1dc592972c",
+    "url": "/RoboFriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "a88177b7b9a705a1c040",
+    "url": "/RoboFriends/static/css/main.d0ef42ce.chunk.css"
+  },
+  {
+    "revision": "449de10d7a1dc592972c",
+    "url": "/RoboFriends/static/js/2.5e932be3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.5e932be3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a88177b7b9a705a1c040",
+    "url": "/RoboFriends/static/js/main.8636c292.chunk.js"
+  },
+  {
+    "revision": "b1f5c836e741cacb5892",
+    "url": "/RoboFriends/static/js/runtime-main.742f4f90.js"
+  }
+]);
